64c1929e63088dfaa73999b4edafd2eee3468ad8
sh8941 <zshamoonhaider@gmail.com>
2026-02-23 17:42:37 +0530

    few more lines added

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello{
     public static void main(String[] args) {
         System.out.println("Hello world...");
+        System.out.println("Leaning git");
     }
 }
+// git diff: is a command that show different between previous and current file (works only between a commited and uncommited)
